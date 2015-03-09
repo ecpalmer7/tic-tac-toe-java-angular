bb111e721dab9c3931da5c4d5f19aa887f8bb585
Tim David <tim.c.david@gmail.com>
2015-03-09 14:33:28 -0500

    controller as syntax

--- src/main/resources/public/js/GameController.js
+++ src/main/resources/public/js/GameController.js
@@ -1,31 +1,33 @@
 
 
-angular.module('gameApp.controllers').controller("GameController", function Hello($scope, $http, $rootScope) {
+angular.module('gameApp.controllers').controller("GameController", function Hello($http, $rootScope) {
+	
+	var vm = this;
 	
 	// added
-	$scope.gameId = null;
-	$scope.game = null;
-	$scope.levelOptions = [{'value' : 'HARD', 'text' : 'Hard'}, 
+	vm.gameId = null;
+	vm.game = null;
+	vm.levelOptions = [{'value' : 'HARD', 'text' : 'Hard'}, 
 	                       {'value' : 'MEDIUM', 'text' : 'Medium'}, 
 	                       {'value' : 'EASY', 'text' : 'Easy'}];
-	$scope.level = 'HARD';
+	vm.level = 'HARD';
 	
 	// original
-	$scope.userLetter = 'o';
-    $scope.systemLetter = 'x';
+	vm.userLetter = 'o';
+	vm.systemLetter = 'x';
 
-    $scope.turn = 0;
+	vm.turn = 0;
 
-    $scope.gameover = false;
-    $scope.winmessage = '';
+	vm.gameover = false;
+	vm.winmessage = '';
 
-    $scope.startOptions = [{'value' : 'system', 'text' : 'AI'}, 
+	vm.startOptions = [{'value' : 'system', 'text' : 'AI'}, 
                            {'value' : 'user', 'text' : 'Me'}];
-    $scope.start = 'system';
+	vm.start = 'system';
 
-    $scope.isGameStarted = false;
+	vm.isGameStarted = false;
 	
-	$scope.rows = [
+	vm.rows = [
 	               [
 	                   {'id' : '11','letter': '','class': 'box'},
 	                   {'id' : '12','letter': '','class': 'box'},
@@ -42,8 +44,7 @@ angular.module('gameApp.controllers').controller("GameController", function Hell
 	                   {'id' : '33','letter': '','class': 'box'}
 	               ]
 	           ];
-	
-	
+		
 	function convertId(id) {
 		// incoming form 11,22,33,etc.
 		var row = id.charAt(0);
@@ -53,106 +54,105 @@ angular.module('gameApp.controllers').controller("GameController", function Hell
 	}
 	
 	function sendUserMove(move) {
-		$http.put('/api/games/'+ $scope.gameId +'/turn', move).success(function(data) {
+		$http.put('/api/games/'+ vm.gameId +'/turn', move).success(function(data) {
     		
-			$scope.game = data;
+			vm.game = data;
 			
 			// returned data is the current game data
 			if (data.status == "OPEN") {
 				getNextMove();
 			} else {
-				$scope.setUserTurn();
+				vm.setUserTurn();
 	    		$rootScope.$broadcast("gamesRefresh");
-	    	}
-	          
+	    	}	          
 	    });
 	}
 	
-	$scope.markUserClick = function(column) {
+	vm.markUserClick = function(column) {
 		
-		if ($scope.game) {
-			if ($scope.game.status |= "OPEN") {
+		if (vm.game) {
+			if (vm.game.status |= "OPEN") {
 				return;
 			}
 		}
-        if($scope.turn == 1 && column.letter == '') {
-            column.letter = $scope.userLetter;
-            $scope.turn = 0;
+        if(vm.turn == 1 && column.letter == '') {
+            column.letter = vm.userLetter;
+            vm.turn = 0;
             
             var position = convertId(column.id);
-            var move = {'position': position, 'player': $scope.userLetter.toUpperCase()};
+            var move = {'position': position, 'player': vm.userLetter.toUpperCase()};
             
             sendUserMove(move);           
         }
         
     };
     
-    $scope.checkWin = function(letter) {
+    vm.checkWin = function(letter) {
     	
-    	if ($scope.game) {
-    		if (($scope.game.status == "WIN") && ($scope.game.winner == letter.toUpperCase())) {
+    	if (vm.game) {
+    		if ((vm.game.status == "WIN") && (vm.game.winner == letter.toUpperCase())) {
     			return true;
     		}
     	}
     	return false;
     }
     
-   $scope.checkDraw = function() {
+    vm.checkDraw = function() {
     	
-    	if ($scope.game) {
-    		if ($scope.game.status == "DRAW") {
+    	if (vm.game) {
+    		if (vm.game.status == "DRAW") {
     			return true;
     		}
     	}
     	return false;
     }
    
-    $scope.setUserTurn = function() {
-        if($scope.checkWin($scope.systemLetter)) {
-            $scope.winmessage = 'I WIN!';
-            $scope.gameover = true;
-        } else if ($scope.checkDraw()) {
-            $scope.winmessage = 'DRAW!';
-            $scope.gameover = true;
+    vm.setUserTurn = function() {
+        if(vm.checkWin(vm.systemLetter)) {
+        	vm.winmessage = 'I WIN!';
+        	vm.gameover = true;
+        } else if (vm.checkDraw()) {
+        	vm.winmessage = 'DRAW!';
+        	vm.gameover = true;
         } else {
-            $scope.turn = 1;
+        	vm.turn = 1;
         }
         return true;
     };
     
     function checkGameStatus() {
-    	$http.get('/api/games/'+ $scope.gameId ).success(function(data) {
-    		$scope.game = data;
+    	$http.get('/api/games/'+ vm.gameId ).success(function(data) {
+    		vm.game = data;
     		
     		if (data.status != "OPEN") {
     			$rootScope.$broadcast("gamesRefresh");
     		}
-    		$scope.setUserTurn();
+    		vm.setUserTurn();
     	});
     };
     
     function getNextMove() {
-    	$http.put('/api/games/'+ $scope.gameId +'/autoturn').success(function(data) {
+    	$http.put('/api/games/'+ vm.gameId +'/autoturn').success(function(data) {
     		
-    		$scope.game = data;
+    		vm.game = data;
     		
  	        var row = data.position.row;
 	        var column = data.position.column;
 	        var player = data.player;
 	           
-	        $scope.rows[row-1][column-1].letter = player.toLowerCase();   
+	        vm.rows[row-1][column-1].letter = player.toLowerCase();   
 	           
 	        checkGameStatus();
 	    });
     };
     
     function newGame( ) {
-    	var postData = {level: $scope.level, computerPlaysAs: $scope.systemLetter.toUpperCase()};
+    	var postData = {level: vm.level, computerPlaysAs: vm.systemLetter.toUpperCase()};
     	$http.post('/api/games', postData, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
             params: postData
         }).success(function(data) {
-	           $scope.gameId = data.id;
+        	vm.gameId = data.id;
 	           
 	           // if turn is system, then get next move after start
 	           if (data.computerPlaysAs == 'X') {
@@ -163,28 +163,27 @@ angular.module('gameApp.controllers').controller("GameController", function Hell
 	    });
     };
     
-    $scope.startGame = function() {
-        $scope.gameover = false;
-        $scope.game = null;
+    vm.startGame = function() {
+    	vm.gameover = false;
+    	vm.game = null;
         
-        angular.forEach($scope.rows, function(row) {
+        angular.forEach(vm.rows, function(row) {
             row[0].letter = row[1].letter = row[2].letter = '';
             row[0].class = row[1].class = row[2].class = 'box';
         });
-        $scope.isGameStarted = true;
-        if($scope.start == 'system') {
-            $scope.turn = 0;
-            $scope.userLetter = 'o';
-            $scope.systemLetter = 'x';
+        vm.isGameStarted = true;
+        if(vm.start == 'system') {
+        	vm.turn = 0;
+        	vm.userLetter = 'o';
+        	vm.systemLetter = 'x';
             
         }
         else {
-            $scope.turn = 1;
-            $scope.userLetter = 'x';
-            $scope.systemLetter = 'o';
+        	vm.turn = 1;
+        	vm.userLetter = 'x';
+        	vm.systemLetter = 'o';
         }
         
-        // if turn is system, then get next move after start
         newGame();  
     };
     
