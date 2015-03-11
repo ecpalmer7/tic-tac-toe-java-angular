585925794e38fbacc59ba684baf5ad7e35c814e4
Tim David <tim.c.david@gmail.com>
2015-03-10 22:37:19 -0500

    add data factory

--- src/main/resources/public/js/DataFactory.js
+++ src/main/resources/public/js/DataFactory.js
@@ -12,17 +12,25 @@ angular.module('gameApp.controllers')
         return $http.get(urlBase + '/' + id);
     };
 
-    dataFactory.insertCustomer = function (cust) {
-        return $http.post(urlBase, cust);
-    };
-
-    dataFactory.updateCustomer = function (cust) {
-        return $http.put(urlBase + '/' + cust.ID, cust)
-    };
-
     dataFactory.deleteGame = function (id) {
         return $http.delete(urlBase + '/' + id);
     };
+    
+    dataFactory.getNextMove = function () {
+    	return $http.put(urlBase + '/'+ vm.gameId +'/autoturn');
+    }
+    
+    dataFactory.sendUserMove = function (move) {
+    	return $http.put(urlBase + '/'+ vm.gameId +'/turn', move);
+    }
+    
+    dataFactory.createNewGame = function (level, computerPlaysAs) {
+    	var postData = {'level': level, 'computerPlaysAs': computerPlaysAs.toUpperCase()};
+    	return $http.post(urlBase, postData, {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            params: postData
+        });
+    }
 
     return dataFactory;
 }]);
