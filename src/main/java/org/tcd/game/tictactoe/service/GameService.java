b7aa0eedea27b91da0adb5f5fbbad8ccdda16e9f
Tim David <tim.c.david@gmail.com>
2015-03-13 20:09:50 -0500

    remove gameservice interface

--- src/main/java/org/tcd/game/tictactoe/service/GameService.java
+++ src/main/java/org/tcd/game/tictactoe/service/GameService.java
@@ -3,16 +3,67 @@ package org.tcd.game.tictactoe.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.tcd.game.tictactoe.domain.Game;
 import org.tcd.game.tictactoe.domain.Level;
 import org.tcd.game.tictactoe.domain.Move;
 import org.tcd.game.tictactoe.domain.Player;
+import org.tcd.game.tictactoe.domain.Status;
+import org.tcd.game.tictactoe.store.GameStore;
 
-public interface GameService {
-	Game newGame(Level level, Player computerPlaysAs);	
-	Optional<Game> find(Long id);
-	Game addMove(Game game, Move move);
-	Move nextMove(Game game);
-	List<Game> getGames();
-	void delete(Long id);
+@Service
+public class GameService  {
+
+	@Autowired
+	GameStore store;
+	
+	@Autowired
+	GameFactory factory;
+	
+	public GameService() {}
+	
+	public Game addMove(Game state, Move move) {
+		Game game = gameLogic(state).addMove(move);
+		
+		// check for draw/win
+		if (gameLogic(game).isDraw()) {
+			game.setStatus(Status.DRAW);
+		} else if (gameLogic(game).isWinner(Player.X)) {
+			game.setStatus(Status.WIN);
+			game.setWinner(Player.X);
+		} else if (gameLogic(game).isWinner(Player.O)) {
+			game.setStatus(Status.WIN);
+			game.setWinner(Player.O);
+		}
+		store.save(game);
+		return game;
+	}
+
+	public Move nextMove(Game game) {
+		return gameLogic(game).nextMove();		
+	}
+
+	public Game newGame(Level level, Player computerPlaysAs) {
+		Game game = factory.createGame(level, computerPlaysAs);
+		store.save(game);
+		return game;
+	}
+	
+	public List<Game> getGames() {
+		return store.getGames();
+	}
+
+	public Optional<Game> find(Long id) {
+		return store.get(id);
+	}
+	
+	public void delete(Long id) {
+		store.delete(id);
+	}
+
+	protected GameLogic gameLogic (Game game) {
+		return new GameLogic(game);
+	}
+
 }
--- src/test/java/org/tcd/game/tictactoe/service/GameServiceTest.java
+++ src/test/java/org/tcd/game/tictactoe/service/GameServiceTest.java
@@ -6,22 +6,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.tcd.game.tictactoe.domain.Game;
 import org.tcd.game.tictactoe.domain.Level;
+import org.tcd.game.tictactoe.domain.Move;
 import org.tcd.game.tictactoe.domain.Player;
 import org.tcd.game.tictactoe.domain.Position;
 
 
-public class GameServiceImplTest {
+public class GameServiceTest {
 
-	GameServiceImpl service;
+	GameService service;
 	
 	@Before
 	public void setup() {
-		service = new GameServiceImpl();
+		service = new GameService();
 	}
 	
 	@Test
 	public void testNextMove() {
-		assertEquals(new Position(1,1), service.nextMove(emptyGame()));
+		assertEquals(new Move( new Position(1,1), Player.X),  service.nextMove(emptyGame()));
 	}
 	
 	private Game emptyGame() {
