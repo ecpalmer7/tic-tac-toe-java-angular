3c92ca3ed7004e32db0df3a5e27f55a0a2b6185b
Tim David <tim.c.david@gmail.com>
2015-04-03 18:43:47 -0500

    combine GameUtil with GameLogic

--- src/main/java/org/tcd/game/tictactoe/service/GameLogic.java
+++ src/main/java/org/tcd/game/tictactoe/service/GameLogic.java
@@ -1,5 +1,7 @@
 package org.tcd.game.tictactoe.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -11,9 +13,8 @@ import org.tcd.game.tictactoe.domain.Player;
 import org.tcd.game.tictactoe.domain.Position;
 import org.tcd.game.tictactoe.domain.Status;
 
-import static org.tcd.game.tictactoe.service.GameUtil.winningPositions;
-import static org.tcd.game.tictactoe.service.GameUtil.openPositions;
-
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class GameLogic  {
 
@@ -51,7 +52,7 @@ public class GameLogic  {
 	}
 
 	boolean isWinner(Player player) {
-		return GameUtil.isWinner(game.movesAsMap(), player);
+		return isWinner(game.movesAsMap(), player);
 	}
 	
 	boolean isInPosition(Player player, int row, int col) {
@@ -251,5 +252,69 @@ public class GameLogic  {
 				  openPosition(2,3),
 				  openPosition(3,2));
 				
+	}
+	
+	// static methods
+	
+	static List<Position> getAllPositions() {
+		List<Position> positions = new ArrayList<Position>();
+		for (int row = 1; row <= 3; row++) {
+			for (int col = 1; col <= 3; col++) {
+				positions.add(new Position(row, col));
+			}
+		}
+		return positions;
+	}
+	
+	static List<List<Position>> getWinningCombos() {
+		List<List<Position>> wins = new ArrayList<List<Position>>();
+		
+		wins.add(asList(new Position(1,1), new Position(1,2), new Position(1,3)));
+		wins.add(asList(new Position(2,1), new Position(2,2), new Position(2,3)));
+		wins.add(asList(new Position(3,1), new Position(3,2), new Position(3,3)));
+		
+		wins.add(asList(new Position(1,1), new Position(2,1), new Position(3,1)));
+		wins.add(asList(new Position(1,2), new Position(2,2), new Position(3,2)));
+		wins.add(asList(new Position(1,3), new Position(2,3), new Position(3,3)));
+		
+		wins.add(asList(new Position(1,1), new Position(2,2), new Position(3,3)));
+		wins.add(asList(new Position(3,1), new Position(2,2), new Position(1,3)));
+		
+		return wins;	
+	}
+		
+	static boolean matches(Map<Position, Player> moves, Player player,  List<Position> winningCombo) {
+		return winningCombo.stream()
+					.allMatch(position -> moves.get(position) == player);
+	}
+	
+	static boolean isWinner(Map<Position, Player> moves, Player player) {
+		return getWinningCombos().stream()
+								.anyMatch(combo -> matches(moves, player, combo));
+	}
+		
+	static boolean isWinnerWithMove(Map<Position, Player> moves, Player player, Position position) {
+		Map<Position, Player> alteredMoves = new HashMap<Position, Player>(moves);
+		alteredMoves.put(position, player);
+		return getWinningCombos().stream()
+								.anyMatch(combo -> matches(alteredMoves, player, combo));
+	}
+	
+	static boolean isOpen(Map<Position, Player> moves, Position position) {
+		return moves.get(position) == null;
+	}
+	
+	static List<Position> openPositions(Map<Position, Player> moves) {
+		return getAllPositions()
+				.stream()
+				.filter(position -> isOpen(moves, position))
+				.collect(toList());
+	}
+		
+	static List<Position> winningPositions(Player player, Map<Position, Player> moves) {
+		return openPositions(moves)
+				.stream()
+				.filter(position -> isWinnerWithMove(moves, player, position))
+				.collect(toList());
 	}
 }
